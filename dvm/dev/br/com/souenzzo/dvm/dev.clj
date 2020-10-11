bbcd2462040f8e33defcbae02139a9838f16f3bc
Enzzo Cavallo <souenzzo@gmail.com>
2020-10-10 21:50:06 -0300

    WIP - dvm

---
+++ dvm/dev/br/com/souenzzo/dvm/dev.clj
@@ -0,0 +1,30 @@
+(ns br.com.souenzzo.dvm.dev
+  (:require [io.pedestal.http :as http]
+            [br.com.souenzzo.dvm :as dvm]
+            [io.pedestal.http.route :as route]
+            [ring.util.mime-type :as mime]))
+
+(defn index
+  [req]
+  {:body    "<button> ok </button>"
+   :status  200
+   :headers {"Content-Type" (mime/default-mime-types "html")}})
+
+(def routes
+  `#{["/" :get index]})
+
+(defonce state (atom nil))
+(defn -main
+  [& _]
+  (swap! state
+         (fn [st]
+           (some-> st
+                   http/stop)
+           (-> {::http/type   :jetty
+                ::http/port   8080
+                ::http/join?  false
+                ::http/routes #(route/expand-routes @#'routes)}
+               http/default-interceptors
+               http/dev-interceptors
+               http/create-server
+               http/start))))
