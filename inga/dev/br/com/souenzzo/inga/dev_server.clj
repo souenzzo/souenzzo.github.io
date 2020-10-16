ccfc983c567331478a6b383ae54ab9c29e855652
Enzzo Cavallo <souenzzo@gmail.com>
2020-10-16 16:04:24 -0300

    +mutation example

--- inga/dev/br/com/souenzzo/inga/dev_server.clj
+++ inga/dev/br/com/souenzzo/inga/dev_server.clj
@@ -3,8 +3,49 @@
             [io.pedestal.http.route :as route]
             [ring.util.mime-type :as mime]
             [br.com.souenzzo.inga.ring :as ir]
-            [com.wsscode.pathom.connect :as pc])
+            [com.wsscode.pathom.connect :as pc]
+            [io.pedestal.interceptor :as interceptor]
+            [clojure.pprint :as pp])
   (:import (java.nio.charset StandardCharsets)))
+
+
+(pc/defresolver current-value [{::keys [counter]} _]
+  {::current-value @counter})
+
+(pc/defresolver counter-display [_ {::keys [current-value]}]
+  {::counter-display [:p (str current-value)]})
+
+(pc/defresolver counter-controls [_ _]
+  {::counter-controls [:form
+                       {:method "POST"
+                        :action "/app/inc"}
+                       [:input {:type  "submit"
+                                :value "+"}]]})
+
+(pc/defmutation increment [{::keys [counter]} _]
+  {::pc/sym 'app/inc}
+  (swap! counter inc)
+  {})
+
+(pc/defmutation mutate [{:keys [parser path-params]
+                         :as   env} params]
+  {::pc/sym    `ir/mutate
+   ::pc/output [::ir/body
+                ::ir/status
+                ::ir/headers]}
+  (let [tx `[{(~(symbol (:mutation path-params)) ~{})
+              []}]
+        result (parser env tx)]
+    {::ir/body    nil
+     ::ir/status  303
+     ::ir/headers {"Location" (-> env :headers (get "referer" "/"))}}))
+
+(def indexes
+  (pc/register {} [counter-display
+                   current-value
+                   increment
+                   mutate
+                   counter-controls]))
 
 (defn index
   [req]
@@ -14,41 +55,44 @@
                [:link {:rel "icon" :href "data:"}]
                [:title "ingá!"]]
               [:body
-               [::hello {}]]]
+               [::counter-display {}]
+               [::counter-controls {}]]]
    :headers  {"Content-Security-Policy" ""
               "Content-Type"            (mime/default-mime-types "html")}
    :status   200})
 
-(pc/defresolver hello [_ _]
-  {::hello [:div
-            [:p "hello"]
-            [::world]]})
-
-(pc/defresolver world [_ _]
-  {::world [:p "world!"]})
-
-(def indexes
-  (pc/register {} [hello world]))
-
-(def connect (ir/interceptor {::pc/indexes indexes}))
+(defonce counter (atom 0))
+(def connect (ir/interceptor {::counter    counter
+                              ::pc/indexes indexes}))
 
 (def routes
-  `#{["/" :get [connect
-                index]]})
-
+  `#{["/" :get [connect index]]
+     ["/*mutation" :post [connect ir/mutate]]})
 
 (defonce state (atom nil))
+
+(def not-found-interceptor
+  (interceptor/interceptor
+    {:name  ::not-found
+     :leave (fn [{:keys [response request] :as ctx}]
+              (if (http/response? response)
+                ctx
+                (assoc ctx :response
+                           {:body    (with-out-str (pp/pprint request))
+                            :headers {"Content-Type" (mime/default-mime-types "txt")}
+                            :status  404})))}))
 
 (defn -main
   [& _]
   (swap! state
          (fn [st]
            (some-> st http/stop)
-           (-> {::http/routes (fn []
-                                (route/expand-routes @(requiring-resolve `routes)))
-                ::http/port   8080
-                ::http/join?  false
-                ::http/type   :jetty}
+           (-> {::http/routes                (fn []
+                                               (route/expand-routes @(requiring-resolve `routes)))
+                ::http/port                  8080
+                ::http/not-found-interceptor not-found-interceptor
+                ::http/join?                 false
+                ::http/type                  :jetty}
                http/default-interceptors
                http/dev-interceptors
                http/create-server
--- inga/src/br/com/souenzzo/inga/ring.clj
+++ inga/src/br/com/souenzzo/inga/ring.clj
@@ -1,6 +1,8 @@
 (ns br.com.souenzzo.inga.ring
   (:require [br.com.souenzzo.inga.connect :as ic]
-            [hiccup2.core :as h]))
+            [hiccup2.core :as h]
+            [com.wsscode.pathom.core :as p]
+            [com.wsscode.pathom.connect :as pc]))
 
 (defn on-response
   [request {::keys [body]
@@ -28,3 +30,24 @@
       (let [env (merge request env)]
         (->> (handler env)
              (on-response env))))))
+
+
+(def parser
+  (p/parser {::p/plugins [(pc/connect-plugin)]
+             ::p/mutate  pc/mutate
+             ::p/env     {::p/reader               [p/map-reader
+                                                    pc/reader2
+                                                    pc/open-ident-reader
+                                                    p/env-placeholder-reader]
+                          ::p/placeholder-prefixes #{">"}}}))
+
+(def mutate
+  {:name  ::mutate
+   :enter (fn [{:keys [request]
+                :as   ctx}]
+            (let [tx `[{(mutate ~{}) [(::body {:pathom/as :body})
+                                      (::headers {:pathom/as :headers})
+                                      (::status {:pathom/as :status})]}]
+                  response (-> (parser request tx)
+                               (get `mutate))]
+              (assoc ctx :response response)))})
