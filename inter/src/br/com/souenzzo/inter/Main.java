9de8d71d14403578121286a9e3905a425109d688
Enzzo Cavallo <souenzzo@gmail.com>
2020-10-19 20:19:23 -0300

    +eql graphs

--- inter/src/br/com/souenzzo/inter/Main.java
+++ inter/src/br/com/souenzzo/inter/Main.java
@@ -1,7 +1,10 @@
 package br.com.souenzzo.inter;
 
+import org.eclipse.jetty.server.*;
+
 public class Main {
     public static void main(String[] argv) {
+        var jetty = new Server();
         System.out.println("ok!");
     }
 }
