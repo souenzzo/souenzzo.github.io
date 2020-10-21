ad949573f1153b605ac977e46b5cfa9a2d336c27
Enzzo Cavallo <souenzzo@gmail.com>
2020-10-20 21:56:59 -0300

    +readPath

--- inter/src/br/com/souenzzo/inter/Main.java
+++ inter/src/br/com/souenzzo/inter/Main.java
@@ -1,54 +1,107 @@
 package br.com.souenzzo.inter;
 
-
-import java.io.IOException;
+import java.io.*;
 import java.net.ServerSocket;
-import java.net.Socket;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
-class DSHttp {
-    public int port;
-    private ServerSocket ss;
-    private ExecutorService es;
-    private Socket client;
-    private final Runnable watch = () -> {
-        try {
-            client = ss.accept();
-            es.submit(this.handler(client));
-        } catch (IOException e) {
+record Request(String method, String path, String version,
+               Map<String, List<String>> headers,
+               InputStream body) {
+    public static Request from(InputStream is) throws IOException {
+        var pis = new PushbackInputStream(is);
+        return new Request(
+                readMethod(pis),
+                readPath(pis),
+                readVersion(pis),
+                readHeaders(pis),
+                is
+        );
+    }
+
+    private static Map<String, List<String>> readHeaders(InputStream is) {
+        return new HashMap<>();
+    }
+
+    private static String readVersion(PushbackInputStream pis) {
+        return "ok";
+    }
+
+    private static String readMethod(PushbackInputStream pis) throws IOException {
+        var sb = new StringBuilder();
+        while (true) {
+            var x = pis.read();
+            if (Character.isWhitespace(x)) {
+                return sb.toString();
+            }
+            sb.append(x);
         }
-        es.submit(this.watch);
-    };
 
-    public void start() throws IOException {
-        es = Executors.newFixedThreadPool(2);
-        ss = new ServerSocket(port);
-        es.submit(this.watch);
     }
 
-    @org.jetbrains.annotations.NotNull
-    @org.jetbrains.annotations.Contract(pure = true)
-    private Runnable handler(Socket client) {
-        return () -> {
+    private static String readPath(PushbackInputStream pis) throws IOException {
+        // EOL in HTTP: \r\n
+        // \return  = 13 = 0x0D
+        // \newline = 10 = 0x0A
+        // \space   = 32 = 0x20
+        var sb = new StringBuilder();
+        while (true) {
+            var x = pis.read();
+            if (x == 13) {
+                var y = pis.read();
+                if (y == 10) {
+                    var idx = sb.lastIndexOf(" ");
+                    var buff = new byte[idx];
+                    if (idx < 0) {
+                        // no version
+                        pis.unread(y);
+                        pis.unread(x);
+                        return sb.toString();
+                    }
 
-        };
+                    sb.getChars(idx, sb.length(), buff, 0);
+                    pis.unread(buff);
+                    pis.unread(y);
+                    pis.unread(x);
+                    return sb.toString();
+                }
+                sb.append(x);
+                sb.append(y);
+            } else {
+                sb.append(x);
+            }
+        }
     }
+}
 
-    public void setSs(ServerSocket ss) {
-        this.ss = ss;
-    }
+class EchoHttpServer {
+    void start() throws IOException {
+        try (final var ss = new ServerSocket(8080)) {
+            while (true) {
+                final var sc = ss.accept();
+                final var is = sc.getInputStream();
+                final var os = sc.getOutputStream();
+                // final var request = new String(is.readAllBytes());
+                // System.out.println(request);
+                os.write(String.join("\r\n",
+                        "HTTP/1.1 200 OK",
+                        "Date: Tue, 20 Oct 2020 21:16:08 GMT",
+                        "Server: dshttp/0",
+                        "",
+                        "ok!").getBytes());
+                os.close();
+                is.close();
+                sc.close();
+            }
 
-    public void setEs(ExecutorService es) {
-        this.es = es;
-    }
-
-    public void setClient(Socket client) {
-        this.client = client;
+        }
     }
 }
 
 public class Main {
-    public static void main(String[] argv) {
+    public static void main(String[] argv) throws IOException {
+        (new EchoHttpServer()).start();
     }
 }
+
