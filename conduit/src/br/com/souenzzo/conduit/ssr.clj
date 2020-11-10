d72d95fe0641873876f2cbf8144a244a0df07499
Enzzo Cavallo <souenzzo@gmail.com>
2020-11-10 15:35:49 -0300

    Wip

--- conduit/src/br/com/souenzzo/conduit/ssr.clj
+++ conduit/src/br/com/souenzzo/conduit/ssr.clj
@@ -1,12 +1,71 @@
 (ns br.com.souenzzo.conduit.ssr
-  (:require [io.pedestal.http :as http]))
+  (:require [io.pedestal.http :as http]
+            [hiccup2.core :as h]
+            [hiccup.util :as hu]
+            [ring.util.mime-type :as mime]
+            [io.pedestal.http.route :as route]
+            [clojure.pprint :as pp])
+  (:import (java.net URI)))
 
-(defn index
+(defn index-a
   [req]
-  {:status 200})
+  [:html
+   [:head]
+   [:body
+    [:div "ok!"]
+    [:div
+     [:a {:href (URI. (route/url-for ::index-b))}
+      "bb"]]
+    [:a {:href (route/url-for ::index-b)}
+     "b"]]])
+
+
+(defn index-b
+  [req]
+  [:html
+   [:head]
+   [:body
+    [:div "ok!"]
+    [:div
+     [:a {:href (URI. (route/url-for ::index-a))}
+      "aa"]]
+    [:a {:href (route/url-for ::index-a)}
+     "a"]]])
+
+
+(def hiccup->response
+  {:name  ::hiccup
+   :leave (fn [{:keys [response]
+                :as   ctx}]
+            (if (and (coll? response)
+                     (not (map? response)))
+              (assoc ctx :response {:body   response
+                                    :status 200})
+              ctx))})
+
+(def render-hiccup
+  {:name  ::render-hiccup
+   :leave (fn [{:keys [response]
+                :as   ctx}]
+            (if-let [body (:body response)]
+              (try
+                (let [url (some->> (get-in ctx [:request :headers "referer"])
+                                   (URI.))]
+                  (pp/pprint (bean url))
+                  (-> ctx
+                      (assoc-in [:response :body] (binding [hu/*base-url* (get-in ctx [:request :headers "referer"])]
+                                                    (prn (-> ctx :request :headers))
+                                                    (->> body
+                                                         (h/html {:mode :html})
+                                                         (str "<!DOCTYPE html>\n"))))
+                      (assoc-in [:response :headers "Content-Type"] (mime/default-mime-types "html"))))
+                (catch Throwable ex                         ;; not a valid html body
+                  ctx))
+              ctx))})
 
 (def routes
-  `#{["/" :get index]})
+  `#{["/a" :get [render-hiccup hiccup->response index-a]]
+     ["/b" :get [render-hiccup hiccup->response index-b]]})
 
 (def service
   (-> {::http/join?  false
--- conduit/test/br/com/souenzzo/conduit/ssr_test.clj
+++ conduit/test/br/com/souenzzo/conduit/ssr_test.clj
@@ -4,15 +4,40 @@
             [midje.sweet :refer [fact =>]]
             [io.pedestal.test :refer [response-for]]
             [io.pedestal.http :as http]
-            [io.pedestal.http.route :as route]))
+            [io.pedestal.http.route :as route]
+            [cheshire.core :as json]
+            [ring.util.mime-type :as mime]))
+
+(defn ->app
+  [{::http/keys [routes]
+    :as         service-map}]
+  (-> service-map
+      http/create-servlet
+      (assoc ::route/form-actions (-> routes
+                                      route/expand-routes
+                                      route/form-action-for-routes))))
+
+(defn request
+  [{::http/keys  [service-fn]
+    ::route/keys [form-actions]} route-name & opts]
+  (let [{:keys [method action]} (apply form-actions route-name opts)
+        {:keys [body headers json]} opts
+        args (sequence cat (merge (when body
+                                    {:body body})
+                                  (when headers
+                                    {:headers headers})
+                                  (when json
+                                    {:body    (json/generate-string json)
+                                     :headers (assoc headers "Content-Type" (mime/default-mime-types "json"))})))
+        {:keys [body]
+         :as   response} (apply response-for service-fn (keyword method) action args)]
+    (try
+      (assoc response :json (json/parse-string body keyword))
+      (catch Throwable ex
+        response))))
 
 (deftest hello
-  (let [service-fn (-> conduit/service
-                       http/create-servlet
-                       ::http/service-fn)
-        url-for (route/url-for-routes (route/expand-routes conduit/routes))]
+  (let [app (->app conduit/service)]
     (fact
-      (-> (response-for service-fn :get
-                        (url-for ::conduit/index))
-          :status)
+      (-> (request app ::conduit/index))
       => 200)))
