abd7f72f34956f3976c2713ee793d2a51ca4abb6
Enzzo Cavallo <souenzzo@gmail.com>
2020-11-22 16:02:49 -0300

    WIP

--- openapi-connect/src/br/com/souenzzo/openapi_connect.clj
+++ openapi-connect/src/br/com/souenzzo/openapi_connect.clj
@@ -1,1 +1,36 @@
-(ns br.com.souenzzo.openapi-connect)
+(ns br.com.souenzzo.openapi-connect
+  (:require [com.wsscode.pathom3.connect.operation :as pco]
+            [clojure.string :as string]))
+
+(defn ->op-name
+  [{::keys [ns path method]}
+   {:strs [operationId]}]
+  (if operationId
+    (symbol ns operationId)
+    (symbol ns (string/join ":" (map munge [path method])))))
+
+(defn response->attribute
+  [{::keys [ns]} {:strs [schema]}]
+  (some-> schema (get "$ref") (string/split #"/") last (->> (keyword ns))))
+
+(defn operation->docstring
+  [{:strs [summary description]}]
+  (string/join "\n" [summary "" description]))
+
+(defn operation->output
+  [env {:strs [responses]}]
+  (vec (for [[_status {:strs [schema]}] responses
+             :let [ident (response->attribute env schema)]
+             :when (keyword? ident)]
+         ident)))
+
+(defn sync-resolve-fn
+  [{::keys [spec path method] :as env}]
+  (let [operation (get-in spec ["paths" path method])]
+    (pco/resolver
+      {::pco/op-name   (->op-name env operation)
+       ::pco/docstring (operation->docstring operation)
+       ::pco/output    (operation->output env operation)
+       ::pco/resolve   (fn [env input]
+                         (let []
+                           {:ok 42}))})))
--- openapi-connect/test/br/com/souenzzo/openapi_connect_test.clj
+++ openapi-connect/test/br/com/souenzzo/openapi_connect_test.clj
@@ -1,5 +1,21 @@
 (ns br.com.souenzzo.openapi-connect-test
   (:require [clojure.test :refer [deftest]]
             [br.com.souenzzo.openapi-connect :as oc]
-            [midje.sweet :refer [fact =>]]))
+            [midje.sweet :refer [fact =>]]
+            [com.wsscode.pathom.connect :as pc]
+            [com.wsscode.pathom3.connect.operation :as pco]
+            [clojure.data.json :as json]
+            [clojure.java.io :as io]))
+
+(def openapi (json/read (io/reader (io/resource "realworld/api/swagger.json"))))
+
+(deftest simple
+  (let [resolver (oc/sync-resolve-fn
+                   {::oc/spec   openapi
+                    ::oc/path   "/tags"
+                    ::oc/method "get"
+                    ::oc/ns     "gothinkster.realworld.conduit.api"})]
+    (fact
+      (resolver {} {})
+      => {})))
 
