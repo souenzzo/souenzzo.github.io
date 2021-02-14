ef357b55b885b77cb4e59a187d97c925e45bb6b7
Enzzo Cavallo <souenzzo@gmail.com>
2021-02-14 12:24:51 -0300

    Routing ok

--- conduit/src/br/com/souenzzo/conduit/ssr.clj
+++ conduit/src/br/com/souenzzo/conduit/ssr.clj
@@ -12,6 +12,15 @@
             [ring.util.mime-type :as mime])
   (:import (java.nio.charset StandardCharsets)))
 
+(defn href
+  [route-name & others]
+  (let [{:as opts} others]
+    (-> (assoc opts
+          :route-name route-name)
+        (with-meta `{dvm/-render ~(fn [_ _]
+                                    (apply route/url-for route-name others))}))))
+
+
 (defn ui-head
   [_]
   [:head
@@ -35,25 +44,25 @@
     {:class "container"}
     [:a
      {:class "navbar-brand"
-      :href  "#"}
+      :href  (href :conduit.page/home)}
      "conduit"]
     [:ul
      {:class "nav navbar-nav pull-xs-right"}
-     (for [[label route-name] [["Home" :conduit.page/home]
-                               ["Sign in" :conduit.page/login]
-                               ["Sign up" :conduit.page/register]]]
+     (for [[label href] [["Home" (href :conduit.page/home)]
+                         ["Sign in" (href :conduit.page/login)]
+                         ["Sign up" (href :conduit.page/register)]]]
        [:li
         {:class "nav-item"}
         [:a
          {:class "nav-link"
-          :href  "#"}
+          :href  href}
          label]])
      #_[:li.nav-item
-        [:a.nav-link {:href (hiete/href :conduit.page/editor)}
+        [:a.nav-link {:href "#"}
          [:i.ion-compose]
          (str " " "New Post")]]
      #_[:li.nav-item
-        [:a.nav-link {:href (hiete/href :conduit.page/settings)}
+        [:a.nav-link {:href "#"}
          [:i.ion-gear-a]
          (str " " "Settings")]]]]])
 
@@ -73,164 +82,183 @@
       "Thinkster"]
      ". Code &amp; design licensed under MIT."]]])
 
+(defn ui-account-form
+  [req {:keys [fields label tips]}]
+  [:div
+   {:class "auth-page"}
+   [:div
+    {:class "container page"}
+    [:div
+     {:class "row"}
+     [:div
+      {:class "col-md-6 offset-md-3 col-xs-12"}
+      [:h1
+       {:class "text-xs-center"}
+       label]
+      (for [[label href] tips]
+        [:p
+         {:class "text-xs-center"}
+         [:a {:href href}
+          label]])
+      #_[:ul.error-messages
+         [:li "That email is already taken"]]
+      [:form
+       {}
+       (for [field fields]
+         [:fieldset
+          {:class "form-group"}
+          [:input
+           (merge {:class "form-control form-control-lg"}
+                  field)]])
+       [:button
+        {:class "btn btn-lg btn-primary pull-xs-right"}
+        "Sign in"]]]]]])
+
 (defn ui-register
   [req]
-  {:html   [:html
-            [ui-head]
-            [:body
-             (ui-nav req)
-             [:div.auth-page
-              [:div.container.page
-               [:div.row
-                [:div.col-md-6.offset-md-3.col-xs-12
-                 [:h1.text-xs-center "Sign up"]
-                 [:p.text-xs-center
-                  [:a {:href (hiete/href :conduit.page/login)}
-                   "Have an account?"]]
-                 #_[:ul.error-messages
-                    [:li "That email is already taken"]]
-                 [:form
-                  (hiete/mutation req 'conduit.operation/register)
-                  [:fieldset.form-group
-                   [:input.form-control.form-control-lg
-                    {:type        "text"
-                     :placeholder "Your Name"}]]
-                  [:fieldset.form-group
-                   [:input.form-control.form-control-lg
-                    {:type        "text"
-                     :placeholder "Email"}]]
-                  [:fieldset.form-group
-                   [:input.form-control.form-control-lg
-                    {:type        "password"
-                     :placeholder "Password"}]]
-                  [:button.btn.btn-lg.btn-primary.pull-xs-right "Sign up"]]]]]]
-             (ui-footer req)]]
-   :status 200})
+  {:body    (->> [:html
+                  [ui-head]
+                  [:body
+                   [ui-nav]
+                   [ui-account-form {:fields [{:type        "text"
+                                               :placeholder "Username"}
+                                              {:type        "email"
+                                               :placeholder "email"}
+                                              {:type        "password"
+                                               :placeholder "Password"}]
+                                     :label  "Sign on"
+                                     :tips   {"Have an account?" (href :conduit.page/login)}}]
+                   [ui-footer]]]
+                 (dvm/render-to-string req)
+                 (conj ["<!DOCTYPE html>"])
+                 (string/join "\n"))
+   :headers {"Content-Type" (mime/default-mime-types "html")}
+   :status  200})
 
 
 (defn ui-login
   [req]
-  {:html   [:html
-            (ui-head req)
-            [:body
-             (ui-nav req)
-             [:div.auth-page
-              [:div.container.page
-               [:div.row
-                [:div.col-md-6.offset-md-3.col-xs-12
-                 [:h1.text-xs-center "Sign in"]
-                 [:p.text-xs-center
-                  [:a {:href (hiete/href :conduit.page/register)}
-                   "Need an account?"]]
-                 #_[:ul.error-messages
-                    [:li "That email is already taken"]]
-                 [:form
-                  (hiete/mutation req 'conduit.operation/login)
-                  [:fieldset.form-group
-                   [:input.form-control.form-control-lg
-                    {:type        "text"
-                     :placeholder "Email"}]]
-                  [:fieldset.form-group
-                   [:input.form-control.form-control-lg
-                    {:type        "password"
-                     :placeholder "Password"}]]
-                  [:button.btn.btn-lg.btn-primary.pull-xs-right "Sign in"]]]]]]
-             (ui-footer req)]]
-   :status 200})
+  {:body    (->> [:html
+                  [ui-head]
+                  [:body
+                   [ui-nav]
+                   [ui-account-form {:fields [{:type        "text"
+                                               :placeholder "Username"}
+                                              {:type        "password"
+                                               :placeholder "Password"}]
+                                     :label  "Sign in"
+                                     :tips   {"Need an account?" (href :conduit.page/register)}}]
+                   [ui-footer]]]
+                 (dvm/render-to-string req)
+                 (conj ["<!DOCTYPE html>"])
+                 (string/join "\n"))
+   :headers {"Content-Type" (mime/default-mime-types "html")}
+   :status  200})
+
+(defn ui-body-home
+  [env]
+  (let [{:conduit.feed/keys [articles tags]} (p.eql/process env [{:conduit.feed/articles [:conduit.article/tags
+                                                                                          :conduit.article/title
+                                                                                          :conduit.article/description
+                                                                                          :conduit.article/slug
+                                                                                          :conduit.article/favorites-count
+                                                                                          :conduit.article/created-at
+                                                                                          {:conduit.article/author [:conduit.profile/username
+                                                                                                                    :conduit.profile/image]}]}
+                                                                 {:conduit.feed/tags [:conduit.tag/tag]}])]
+    [:div
+     {:class "home-page"}
+     [:div
+      {:class "banner"}
+      [:div
+       {:class "container"}
+       [:h1
+        {:class "logo-font"}
+        "conduit"]
+       [:p "A place to share your knowledge."]]]
+     [:div
+      {:class "container page"}
+      [:div
+       {:class "row"}
+       [:div
+        {:class "col-md-9"}
+        [:div
+         {:class "feed-toggle"}
+         [:ul
+          {:class "nav nav-pills outline-active"}
+          [:li
+           {:class "nav-item"}
+           [:a
+            {:class " nav-link disabled"
+             :href  "#"}
+            "Your Feed"]]
+          [:li
+           {:class "nav-item"}
+           [:a
+            {:class "nav-link active"
+             :href  "#"}
+            "Global Feed"]]]]
+        (for [{:conduit.article/keys [tags title description slug favorites-count author created-at]} articles
+              :let [{:conduit.profile/keys [username image]} author]]
+          [:div
+           {:class "article-preview"}
+           [:div
+            {:class "article-meta"}
+            [:a {:href "#"}
+             [:img {:src image}]]
+            [:div
+             {:class "info"}
+             [:a
+              {:class "author"
+               :href  "#"}
+              username]
+             [:span
+              {:class "date"}
+              (str created-at)]]
+            [:button
+             {:class "btn btn-outline-primary btn-sm pull-xs-right"}
+             [:i
+              {:class "ion-heart"}]
+             favorites-count]]
+           [:a
+            {:class "preview-link"
+             :href  "#"}
+            [:h1 title]
+            [:p description]
+            [:span "Read more..."]
+            [:ul
+             {:class "tag-list"}
+             (for [{:conduit.tag/keys [tag]} tags]
+               [:li
+                {:class "tag-default tag-pill tag-outline ng-binding ng-scope"}
+                tag])]]])]
+       [:div
+        {:class "col-md-3"}
+        [:div
+         {:class "sidebar"}
+         [:p "Popular Tags"]
+         [:div
+          {:class "tag-list"}
+          (for [{:conduit.tag/keys [tag]} tags]
+            [:a
+             {:class "tag-pill tag-default"
+              :href  "#"}
+             tag])]]]]]]))
 
 
 (defn ui-home
   [req]
-  (let [{:conduit.feed/keys [tags articles]} {}
-        ui [:html
-            [ui-head]
-            [:body
-             [ui-nav]
-             [:div
-              {:class "home-page"}
-              [:div
-               {:class "banner"}
-               [:div
-                {:class "container"}
-                [:h1
-                 {:class "logo-font"}
-                 "conduit"]
-                [:p "A place to share your knowledge."]]]
-              [:div
-               {:class "container page"}
-               [:div
-                {:class "row"}
-                [:div
-                 {:class "col-md-9"}
-                 [:div
-                  {:class "feed-toggle"}
-                  [:ul
-                   {:class "nav nav-pills outline-active"}
-                   [:li
-                    {:class "nav-item"}
-                    [:a
-                     {:class " nav-link disabled"
-                      :href  "#"}
-                     "Your Feed"]]
-                   [:li
-                    {:class "nav-item"}
-                    [:a
-                     {:class "nav-link active"
-                      :href  "#"}
-                     "Global Feed"]]]]
-                 (for [{:conduit.article/keys [tags title description slug favorites-count
-                                               author created-at]} articles
-                       :let [{:conduit.profile/keys [username image]} author]]
-                   [:div
-                    {:class "article-preview"}
-                    [:div
-                     {:class "article-meta"}
-                     [:a {:href "#"}
-                      [:img {:src image}]]
-                     [:div
-                      {:class "info"}
-                      [:a
-                       {:class "author"
-                        :href  "#"}
-                       username]
-                      [:span
-                       {:class "date"}
-                       (str created-at)]]
-                     [:button
-                      {:class "btn btn-outline-primary btn-sm pull-xs-right"}
-                      [:i
-                       {:class "ion-heart"}]
-                      favorites-count]]
-                    [:a
-                     {:class "preview-link"
-                      :href  "#"}
-                     [:h1 title]
-                     [:p description]
-                     [:span "Read more..."]
-                     [:ul
-                      {:class "tag-list"}
-                      (for [{:conduit.tag/keys [tag]} tags]
-                        [:li
-                         {:class "tag-default tag-pill tag-outline ng-binding ng-scope"}
-                         tag])]]])]
-                [:div
-                 {:class "col-md-3"}
-                 [:div
-                  {:class "sidebar"}
-                  [:p "Popular Tags"]
-                  [:div
-                   {:class "tag-list"}
-                   (for [{:conduit.tag/keys [tag]} tags]
-                     [:a
-                      {:class "tag-pill tag-default"
-                       :href  "#"}
-                      tag])]]]]]]
-             [ui-footer]]]
-        html (dvm/render-to-string req ui)]
-    {:body    (string/join "\n" ["<!DOCTYPE html>"
-                                 html])
-     :headers {"Content-Type" (mime/default-mime-types "html")}
-     :status  200}))
+  {:body    (->> [:html
+                  [ui-head]
+                  [:body
+                   [ui-nav]
+                   [ui-body-home]
+                   [ui-footer]]]
+                 (dvm/render-to-string req)
+                 (conj ["<!DOCTYPE html>"])
+                 (string/join "\n"))
+   :headers {"Content-Type" (mime/default-mime-types "html")}
+   :status  200})
 
 (pco/defresolver operation:GetArticles []
   {::pco/output [{:conduit.feed/articles [{:conduit.article/author [:conduit.profile/username
@@ -291,7 +319,11 @@
                    :enter (fn [ctx]
                             (update ctx :request merge idx))}
         routes #{["/" :get [merge-env ui-home]
-                  :route-name :conduit.page/home]}]
+                  :route-name :conduit.page/home]
+                 ["/login" :get [merge-env ui-login]
+                  :route-name :conduit.page/login]
+                 ["/register" :get [merge-env ui-register]
+                  :route-name :conduit.page/register]}]
     {::routes routes}))
 
 (pco/defresolver service [{::keys [operations]}]
