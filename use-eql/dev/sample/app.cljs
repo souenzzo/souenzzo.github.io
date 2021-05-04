a638d1ca4b65811999a32a1de2751a78e3709a09
Enzzo Cavallo <souenzzo@gmail.com>
2021-05-03 23:42:03 -0300

    Using reagent

--- use-eql/dev/sample/app.cljs
+++ use-eql/dev/sample/app.cljs
@@ -1,24 +1,18 @@
 (ns sample.app
   (:require [goog.dom :as gdom]
+            [reagent.dom :as rd]
             ["react" :as r]
-            ["react-dom" :as rd]
             [br.com.souenzzo.use-eql :as use-eql]))
 
 (defn Counter
   []
   (let [conn (use-eql/fetch {::use-eql/query [::current-count]})]
-    (r/createElement
-      "div"
-      #js{}
-      (r/createElement
-        "pre"
-        #js{}
-        (pr-str @conn))
-      (r/createElement
-        "button"
-        #js{:onClick (fn []
-                       (use-eql/transact conn `[(increment {})]))}
-        "+"))))
+    [:div
+     [:pre (pr-str @conn)]
+     [:button
+      {:on-click (fn []
+                   (use-eql/transact conn `[(increment {})]))}
+      "+"]]))
 
 (defn TodoApp
   []
@@ -29,56 +23,41 @@
         {:app.todo/keys [todos]
          :as            tree} @conn
         [text set-text] (r/useState "")]
-    (r/createElement
-      "div"
-      #js{}
-      (r/createElement
-        "ul"
-        #js{}
-        (for [{:app.todo/keys [id text]} todos]
-          (r/createElement
-            "li"
-            #js{:key id}
-            text
-            (r/createElement
-              "button"
-              #js{:onClick (fn []
-                             (use-eql/transact conn `[(app.todo/remove ~{:app.todo/id id})]))}
-              "x"))))
+    [:div
+     [:ul
+      (for [{:app.todo/keys [id text]} todos]
+        [:li {:key id}
+         text
+         [:button
+          {:on-click (fn []
+                       (use-eql/transact conn `[(app.todo/remove ~{:app.todo/id id})]))}
+          "x"]])]
+     [:form
+      {:on-submit (fn [e]
+                    (.preventDefault e)
+                    (use-eql/transact conn `[(app.todo/new-todo ~{:app.todo/text text})])
+                    (set-text ""))}
+      [:input {:value     text
+               :on-change (fn [e]
+                            (set-text (-> e .-target .-value)))}]]]))
 
-      (r/createElement
-        "form"
-        #js{:onSubmit (fn [e]
-                        (.preventDefault e)
-                        (use-eql/transact conn `[(app.todo/new-todo ~{:app.todo/text text})])
-                        (set-text ""))}
-        (r/createElement
-          "input"
-          #js{:value text
-              :onChange (fn [e]
-                          (set-text (-> e .-target .-value)))})))))
 
 (defn Root
   []
   (let [[show? set-show?] (r/useState false)]
-    (r/createElement
-      "div"
-      #js{}
-      (r/createElement
-        "button"
-        #js{:onClick (fn []
-                       (set-show? (not show?)))}
-        (pr-str "toggle" [show?]))
-      (when show?
-        (r/createElement Counter))
-      (when show?
-        (r/createElement TodoApp)))))
+    [:div
+     [:button
+      {:onClick (fn []
+                  (set-show? (not show?)))}
+      (pr-str "toggle" [show?])]
+     (when show?
+       [:<>
+        [:f> Counter]
+        [:f> TodoApp]])]))
 
 (defn ^:export start
   [target]
-  (rd/render
-    (r/createElement Root #js {})
-    (gdom/getElement target)))
+  (rd/render [:f> Root] (gdom/getElement target)))
 
 (defn after-load
   [])
